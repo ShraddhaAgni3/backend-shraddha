3101b503390705532b73fdba364e69041063afc8
almanetuk-lab <almanetuk@gmail.com>
2026-02-17 18:08:36 +0530

    plan controller code updated

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -1,31 +1,13 @@
-// import express from "express";
-// import {getNotifications, markAsRead, markNotificationsAsRead } from "../controller/notificationController.js";
-// //import { validateAccessToken } from "../middleware/verfiytoken.js";
-
-// const router = express.Router();
-
-// router.get("/:user_id", getNotifications); // Get Notifications by User ID
-// router.put("/read/:id", markAsRead); // Mark Notification as Read
-// router.put("/read/messages/:userId", markNotificationsAsRead);  // Mark All Notifications as Read for User  
-
-
-// export default router;
-
-
-
-
-
-
 import express from "express";
 // import { getNotifications, markAsRead, markNotificationsAsRead } from "../controller/notificationController.js";
-import {  getUnreadNotifications, getChatMessages, markAsRead, getNotifications, markNotificationsAsRead } from "../controller/notificationController.js";
-import { validateAccessToken } from "../middleware/verfiytoken.js";
+import { markAsRead, getNotifications, markNotificationsAsRead } from "../controller/notificationController.js";
+//import { validateAccessToken } from "../middleware/verfiytoken.js";
 
 const router = express.Router();
 
 //Code Addes 
-router.get("/users", validateAccessToken, getUnreadNotifications);
-router.get("/users/:senderId", validateAccessToken, getChatMessages);
+// router.get("/users", validateAccessToken, getUnreadNotifications);
+// router.get("/users/:senderId", validateAccessToken, getChatMessages);
 
  router.get("/:user_id", getNotifications); // Get Notifications by User ID
  router.put("/read/:id", markAsRead); // Mark Notification as Read               /(4)
